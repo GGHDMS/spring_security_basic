d819381144835ba3eeb907c8f01e2e36d9948dc5
HeoSeokMun <tjrans303@soongsil.ac.kr>
2023-05-08 10:27:11 +0900

    Logout 처리, logoutFilter

--- src/main/java/io/security/spring_security_basic/SecurityConfig.java
+++ src/main/java/io/security/spring_security_basic/SecurityConfig.java
@@ -8,10 +8,13 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.AuthenticationFailureHandler;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.security.web.authentication.logout.LogoutHandler;
+import org.springframework.security.web.authentication.logout.LogoutSuccessHandler;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
 @Configuration
@@ -48,5 +51,24 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .permitAll() // loginPage 모두 접근 가능하게
         ;
 
+        http // 스프링 security post 방식으로 logout 원칙적으로
+                .logout()
+                .logoutUrl("/logout")
+                .logoutSuccessUrl("/login")
+                .addLogoutHandler(new LogoutHandler() {
+                    @Override
+                    public void logout(HttpServletRequest request, HttpServletResponse response, Authentication authentication) {
+                        HttpSession session = request.getSession();
+                        session.invalidate();
+                    }
+                })
+                .logoutSuccessHandler(new LogoutSuccessHandler() {
+                    @Override
+                    public void onLogoutSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
+                        response.sendRedirect("/logout");
+                    }
+                })
+                .deleteCookies("remember-me")
+                ;
     }
 }
